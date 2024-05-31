a56193fdeda20bccd701ef630e196972e4c2870e
Nyakio Maina <nyakiomaina@Nyakios-machine.local>
2024-06-01 01:05:02 +0300

    traits

---
+++ traits/src/main.rs
@@ -0,0 +1,33 @@
+// program to demonstrate use of traits
+// area
+trait Shape {
+    fn area(&self) -> f64;
+}
+
+struct Circle {
+    radius: f64,
+}
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * self.radius * self.radius
+    }
+}
+
+struct Rectangle {
+    width: f64,
+    height: f64,
+}
+
+impl Shape for Rectangle {
+    fn area(&self) -> f64 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let circle = Circle { radius: 5.0 };
+    let rectangle = Rectangle { width: 5.0, height: 5.0 };
+    println!("The area of the circle is {}", circle.area());
+    println!("The area of the rectangle is {}", rectangle.area());
+}
